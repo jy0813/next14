cc6291d113c61bf00472b17b78b277ae5562970f
jy0813 <busbug3@gmail.com>
2023-12-14 20:12:40 +0900

    Feat: PostFrom 추가

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import style from "./home.module.css";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
+import PostForm from "./_component/PostForm";
 
 const Page = () => {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tab />
-        {/* <PostForm />
-      <Post />
+        <PostForm />
+        {/* <Post />
       <Post />
       <Post />
       <Post />
