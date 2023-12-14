3587fb2ee8d45120aaa965595d8d838df8bdbed7
jy0813 <busbug3@gmail.com>
2023-12-14 21:19:45 +0900

    Feat: 탐색페이지 추가

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import style from "./trendSection.module.css";
 import Trend from "@/app/(afterLogin)/_component/Trend";
 
 export default function TrendSection() {
+  const pathName = usePathname();
+  if (pathName === "/explore") return null;
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import style from "./explore.module.css";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
 const Page = () => {
-  return <div>탐색</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 };
 
 export default Page;
