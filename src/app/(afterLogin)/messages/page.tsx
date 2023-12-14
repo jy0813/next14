607e4d30d6fd23169f8a1356091cd60d2c5f69d4
jy0813 <busbug3@gmail.com>
2023-12-14 23:44:26 +0900

    Feat: messages Page 추가

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import style from "./message.module.css";
+import Room from "@/app/(afterLogin)/messages/_component/Room";
 
-const Page = () => {
-  return <div>메세지</div>;
-};
-
-export default Page;
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
+}
