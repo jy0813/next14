d0a949c2682fe4f31219307d003786458c181e23
jy0813 <busbug3@gmail.com>
2023-12-25 19:40:03 +0900

    Feat: react-query SSR 설정

---
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren, useState } from "react";
+
+function RQProvider({ children }: PropsWithChildren) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={process.env.NEXT_PUBLIC_MODE === "local"}
+      />
+    </QueryClientProvider>
+  );
+}
+
+export default RQProvider;
