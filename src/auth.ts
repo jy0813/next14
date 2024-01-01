4c70c26289ec5b2e83046d411e377651c5c96cc1
jy0813 <busbug3@gmail.com>
2024-01-01 17:18:44 +0900

    Feat: 서버 쿠키 공유

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import cookie from "cookie";
 
 export const {
   handlers: { GET, POST },
@@ -25,11 +26,20 @@ export const {
           }),
         });
 
+        let setCookie = authResponse.headers.get("Set-Cookie");
+        console.log("set-cookie", setCookie);
+        if (setCookie) {
+          const parsed = cookie.parse(setCookie);
+          cookies().set("connect.sid", parsed["connect.sid"], parsed); // 브라우저에 쿠키를 심어주는 것
+        }
+
         if (!authResponse.ok) {
           return null;
         }
 
         const user = await authResponse.json();
+        console.log(authResponse);
+        console.log("user", user);
         return {
           email: user.id,
           name: user.nickname,
